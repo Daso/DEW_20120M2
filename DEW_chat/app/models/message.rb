3f8f8276a1df21ded906bef343b6bb3c581502f0
unknown <pcsidsub@.upc.edu.pe>
2012-03-27 22:52:53 -0500

    test

--- DEW_chat/app/models/message.rb
+++ DEW_chat/app/models/message.rb
@@ -1,2 +1,16 @@
 class Message < ActiveRecord::Base
+	Pusher.app_id = '17623'
+	Pusher.key = 'd9a06adcf44a4cc3ed78'
+	Pusher.secret = 'f553ad7f5fd2310ba5d5'
+	
+	after_create :send_to_pusher
+	
+	def send_to_pusher
+	
+		logger.info "Hi?"
+		Pusher['DEWChat'].trigger("message:create",self.to_json)
+	
+	end
+	
+	
 end
